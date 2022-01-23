013212a311d4e3bfe9d66235d0baead84b87ea23
Shrish Premkrishna <shrishpremkrishna@example.com>
2022-01-22 16:43:58 -0800

    Update testMiniUART.py

--- roboclaw_packet_serial/testMiniUART.py
+++ roboclaw_packet_serial/testMiniUART.py
@@ -4,7 +4,7 @@ import serial
 # sudo chmod 666 /dev/ttyS0
 # sudo chmod 666 /dev/serial0
 
-serialport = serial.Serial("serial0", baudrate=9600, timeout=3.0)
+serialport = serial.Serial("/dev/serial0", baudrate=9600, timeout=3.0)
 print ('latest')
 while True:
     serialport.write("rnSay something:")
