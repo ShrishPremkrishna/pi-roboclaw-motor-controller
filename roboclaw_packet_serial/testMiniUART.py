533c432558e9200a1b46d46996df3d7404219518
Shrish Premkrishna <shrish.premkrishna@gmail.com>
2022-01-22 17:41:53 -0800

    Update testMiniUART.py

--- roboclaw_packet_serial/testMiniUART.py
+++ roboclaw_packet_serial/testMiniUART.py
@@ -8,5 +8,6 @@ serialport = serial.Serial("/dev/serial0", baudrate=9600, timeout=3.0)
 print ('latest')
 while True:
     serialport.write(str.encode('rnSay something:'))
+    serialport.write(bytes([10]))
     rcv = port.read(10)
     serialport.write(str.encode('rnYou sent:' + repr(rcv)))
