8589fec40bb5bda17821acd9386623ce4ae62a7b
ShrishPremkrishna <shrish.premkrishna@gmail.com>
2022-01-22 16:36:09 -0800

    update

--- roboclaw_packet_serial/testMiniUART.py
+++ roboclaw_packet_serial/testMiniUART.py
@@ -5,7 +5,8 @@ import serial
 # sudo chmod 666 /dev/serial0
 
 serialport = serial.Serial("serial0", baudrate=9600, timeout=3.0)
+
 while True:
-    serialport.write("gsm")
-    rcv = port.read(10)
-    serialport.write("sent:" + repr(rcv))
+    serialport.write("rnSay something:")
+    rcv = serialport.read(10)
+    serialport.write("rnYou sent:" + repr(rcv))
