0b6bdf50d203cf5013cb52331536ded7380578c4
Shrish Premkrishna <shrish.premkrishna@gmail.com>
2022-01-23 20:21:39 -0800

    Update ps4-controller.py

--- pygame-ps4/ps4-controller.py
+++ pygame-ps4/ps4-controller.py
@@ -18,59 +18,59 @@ joystick_count = pygame.joystick.get_count()
 print("Number of joysticks: {}".format(joystick_count))
 
 
-# For each joystick:
-for i in range(joystick_count):
-    joystick = pygame.joystick.Joystick(i)
-    joystick.init()
+# # For each joystick:
+# for i in range(joystick_count):
+#     joystick = pygame.joystick.Joystick(i)
+#     joystick.init()
 
-    try:
-        jid = joystick.get_instance_id()
-    except AttributeError:
-        # get_instance_id() is an SDL2 method
-        jid = joystick.get_id()
-    print("Joystick {}".format(jid))
+#     try:
+#         jid = joystick.get_instance_id()
+#     except AttributeError:
+#         # get_instance_id() is an SDL2 method
+#         jid = joystick.get_id()
+#     print("Joystick {}".format(jid))
 
-    # Get the name from the OS for the controller/joystick.
-    name = joystick.get_name()
-    print("Joystick name: {}".format(name))
+#     # Get the name from the OS for the controller/joystick.
+#     name = joystick.get_name()
+#     print("Joystick name: {}".format(name))
 
-    try:
-        guid = joystick.get_guid()
-    except AttributeError:
-        # get_guid() is an SDL2 method
-        pass
-    else:
-        print("GUID: {}".format(guid))
+#     try:
+#         guid = joystick.get_guid()
+#     except AttributeError:
+#         # get_guid() is an SDL2 method
+#         pass
+#     else:
+#         print("GUID: {}".format(guid))
 
-    # Usually axis run in pairs, up/down for one, and left/right for
-    # the other.
-    axes = joystick.get_numaxes()
-    print("Number of axes: {}".format(axes))
+#     # Usually axis run in pairs, up/down for one, and left/right for
+#     # the other.
+#     axes = joystick.get_numaxes()
+#     print("Number of axes: {}".format(axes))
     
 
-    for i in range(axes):
-        axis = joystick.get_axis(i)
-        print("Axis {} value: {:>6.3f}".format(i, axis))
+#     for i in range(axes):
+#         axis = joystick.get_axis(i)
+#         print("Axis {} value: {:>6.3f}".format(i, axis))
     
 
-    buttons = joystick.get_numbuttons()
-    print("Number of buttons: {}".format(buttons))
+#     buttons = joystick.get_numbuttons()
+#     print("Number of buttons: {}".format(buttons))
     
 
-    for i in range(buttons):
-        button = joystick.get_button(i)
-        print("Button {:>2} value: {}".format(i, button))
+#     for i in range(buttons):
+#         button = joystick.get_button(i)
+#         print("Button {:>2} value: {}".format(i, button))
     
 
-    hats = joystick.get_numhats()
-    print("Number of hats: {}".format(hats))
+#     hats = joystick.get_numhats()
+#     print("Number of hats: {}".format(hats))
     
 
-    # Hat position. All or nothing for direction, not a float like
-    # get_axis(). Position is a tuple of int values (x, y).
-    for i in range(hats):
-        hat = joystick.get_hat(i)
-        print("Hat {} value: {}".format(i, str(hat)))
+#     # Hat position. All or nothing for direction, not a float like
+#     # get_axis(). Position is a tuple of int values (x, y).
+#     for i in range(hats):
+#         hat = joystick.get_hat(i)
+#         print("Hat {} value: {}".format(i, str(hat)))
 
 counter = 100
 # -------- Main Program Loop -----------
