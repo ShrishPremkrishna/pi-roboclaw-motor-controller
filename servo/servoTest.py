3281178d6a52ac8c3de20c21d1b762240132d8fb
ShrishPremkrishna <shrish.premkrishna@gmail.com>
2022-01-23 16:33:46 -0800

    Update servoTest.py

--- servo/servoTest.py
+++ servo/servoTest.py
@@ -4,7 +4,7 @@ from time import sleep
 
 factory = PiGPIOFactory()
 
-servo = Servo(17, min_pulse_width=900/1000000, max_pulse_width=2100/1000000, pin_factory=factory, frame_width=20/1000)
+servo = Servo(17, pin_factory=factory)
 
 while True:
     servo.value = -1
