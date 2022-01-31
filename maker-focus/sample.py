583f740521a679361c38f8aab654af5e58432538
ShrishPremkrishna <shrish.premkrishna@gmail.com>
2022-01-30 17:57:01 -0800

    Update sample.py

--- maker-focus/sample.py
+++ maker-focus/sample.py
@@ -87,10 +87,12 @@ if __name__=='__main__':
   pwm.setPWMFreq(50)
   while True:
    # setServoPulse(2,2500)
-    for i in range(500,2500,10):  
+    for i in range(500,2000,10):  
       pwm.setServoPulse(0,i)   
       time.sleep(0.02)     
     
-    for i in range(2500,500,-10):
+    for i in range(2000,500,-10):
       pwm.setServoPulse(0,i) 
       time.sleep(0.02)  
+
+    break
