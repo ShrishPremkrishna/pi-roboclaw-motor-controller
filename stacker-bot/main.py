f3dc003dea6ad972b12c9756f58ac36b3abe5b9f
ShrishPremkrishna <shrish.premkrishna@gmail.com>
2022-02-05 15:28:31 -0800

    added strafe and rotate

--- stacker-bot/main.py
+++ stacker-bot/main.py
@@ -15,6 +15,8 @@ class MyController(Controller):
        print("Goodbye world")
 
    # Event handlers for chassis control
+
+        #Down
     def on_R3_down(self, arg):
         print('R3 down: ' + str(round(arg)))
         roboclaw.ForwardM1(0x80,64)
@@ -22,6 +24,7 @@ class MyController(Controller):
         roboclaw.ForwardM1(0x81,64)
         roboclaw.ForwardM2(0x81,64)
 
+        #Up
     def on_R3_up(self, arg):
         print('R3 up: ' + str(round(arg)))
         roboclaw.BackwardM1(0x80,64)
@@ -29,12 +32,39 @@ class MyController(Controller):
         roboclaw.BackwardM1(0x81,64)
         roboclaw.BackwardM2(0x81,64)
 
+        #Left
     def on_R3_left(self, arg):
         print('R3 left: ' + str(round(arg)))
+        roboclaw.ForwardM1(0x80,64)
+        roboclaw.BackwardM2(0x80,64)
+        roboclaw.BackwardM1(0x81,64)
+        roboclaw.ForwardM2(0x81,64)
 
+        #Right
     def on_R3_right(self, arg):
         print('R3 right: ' + str(round(arg)))
+        roboclaw.BackwardM1(0x80,64)
+        roboclaw.ForwardM2(0x80,64)
+        roboclaw.ForwardM1(0x81,64)
+        roboclaw.BackwardM2(0x81,64)
 
+        #Rotate Left
+    def on_R3_left(self, arg):
+        print('R3 left: ' + str(round(arg)))
+        roboclaw.ForwardM1(0x80,64)
+        roboclaw.BackwardM2(0x80,64)
+        roboclaw.ForwardM1(0x81,64)
+        roboclaw.BackwardM2(0x81,64)
+
+        #Rotate right
+    def on_R3_right(self, arg):
+        print('R3 right: ' + str(round(arg)))
+        roboclaw.BackwardM1(0x80,64)
+        roboclaw.ForwardM2(0x80,64)
+        roboclaw.BackwardM1(0x81,64)
+        roboclaw.ForwardM2(0x81,64)
+
+        #At rest y-axis on r3
     def on_R3_y_at_rest(self):
         print("r3 y rest")
         roboclaw.ForwardM1(0x80,0)
@@ -42,7 +72,16 @@ class MyController(Controller):
         roboclaw.ForwardM1(0x81,0)
         roboclaw.ForwardM2(0x81,0)
      
+        #At rest x-axis on r3
     def on_R3_x_at_rest(self):
+        print("r3 x rest")
+        roboclaw.ForwardM1(0x80,0)
+        roboclaw.ForwardM2(0x80,0)
+        roboclaw.ForwardM1(0x81,0)
+        roboclaw.ForwardM2(0x81,0)
+
+        #At rest x-axis on l3
+    def on_L3_x_at_rest(self):
         print("r3 x rest")
         roboclaw.ForwardM1(0x80,0)
         roboclaw.ForwardM2(0x80,0)
@@ -51,6 +90,10 @@ class MyController(Controller):
    
     def on_share_press(self):
         print("share press")
+        roboclaw.ForwardM1(0x80,0)
+        roboclaw.ForwardM2(0x80,0)
+        roboclaw.ForwardM1(0x81,0)
+        roboclaw.ForwardM2(0x81,0)
 
 
 if __name__ == "__main__":
