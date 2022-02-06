9635b9855dac7ec61fea59250746d5cefb83842d
ShrishPremkrishna <shrish.premkrishna@gmail.com>
2022-02-06 09:17:15 -0800

    Update main.py

--- stacker-bot/main.py
+++ stacker-bot/main.py
@@ -19,17 +19,17 @@ class MyController(Controller):
     # Event handlers for linear slide
 
         #Up
-    def on_up_arrow_press():
+    def on_up_arrow_press(self):
         print("Linear slide Up")
         roboclaw.ForwardM1(0x82,Lspeed)
 
         #Down
-    def on_down_arrow_press():
+    def on_down_arrow_press(self):
         print("Linear slide Down")
         roboclaw.BackwardM1(0x82,Lspeed)
 
         #Release
-    def on_up_down_arrow_release():
+    def on_up_down_arrow_release(self):
         print("Linear slide Stop")
         roboclaw.ForwardM1(0x82,0)
         roboclaw.BackwardM1(0x82,0)
