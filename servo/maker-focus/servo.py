9c20b9dd2f4b94bd6ccc9d466d1a0fb31c95f1f0
Shrish Premkrishna <shrish.premkrishna@gmail.com>
2022-02-06 16:10:37 -0800

    Update servo.py

--- servo/maker-focus/servo.py
+++ servo/maker-focus/servo.py
@@ -72,14 +72,13 @@ class PCA9685:
 
 if __name__=='__main__':
  
-  pwm = PCA9685(0x40, debug=True)
-  pwm.setPWMFreq(50)
-  while True:
-   # setServoPulse(2,2500)
-    for i in range(500,2500,10):  
-      pwm.setServoPulse(0,i)   
-      time.sleep(0.02)     
-    
-    for i in range(2500,500,-10):
-      pwm.setServoPulse(0,i) 
-      time.sleep(0.02)
+    pwm = PCA9685(0x40, debug=True)
+    pwm.setPWMFreq(50)
+
+    for i in range(1000,200,10):  
+        pwm.setServoPulse(0,i)   
+        time.sleep(0.02)     
+
+    for i in range(2000,1000,-10):
+        pwm.setServoPulse(0,i) 
+        time.sleep(0.02)
