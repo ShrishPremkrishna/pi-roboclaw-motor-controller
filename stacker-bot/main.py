d1e8d645a09dd51af9f6053f269c7f7cc76fff35
Shrish Premkrishna <shrish.premkrishna@gmail.com>
2022-02-06 19:54:28 -0800

    Update main.py

--- stacker-bot/main.py
+++ stacker-bot/main.py
@@ -60,7 +60,7 @@ class MyController(Controller):
         print("On Square Press - lower barlift")
         print("Barlift pulse at - " + str(self.barlift_pulse))
         if (self.barlift_pulse > self.barlift_min) :
-            self.barlift_pulse = self.barlift_pulse - 100
+            self.barlift_pulse = self.barlift_pulse - 200
             print("Barlift pulse being set at - " + str(self.barlift_pulse))
             pwm.setServoPulse(self.barlift_channel, self.barlift_pulse) 
             sleep(0.02)
