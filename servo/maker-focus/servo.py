847f326ac923207234b000823590942dead77021
Shrish Premkrishna <shrish.premkrishna@gmail.com>
2022-02-06 17:24:39 -0800

    Update servo.py

--- servo/maker-focus/servo.py
+++ servo/maker-focus/servo.py
@@ -70,25 +70,17 @@ class PCA9685:
     pulse = pulse*4096/20000        #PWM frequency is 50HZ,the period is 20000us
     self.setPWM(channel, 0, int(pulse))
 
-
-  def close(self):
-      print("closing bus")
-      result = self.bus.close()
-      print(result)
-      
-
 if __name__=='__main__':
  
     pwm = PCA9685(0x40, debug=True)
     pwm.setPWMFreq(50)
 
-    for i in range(1000,2000,10):  
+    for i in range(500,2500,10):  
         pwm.setServoPulse(0,i)   
         time.sleep(0.02)     
 
-    for i in range(2000,1000,-10):
+    for i in range(2500,500,-10):
         pwm.setServoPulse(0,i) 
         time.sleep(0.02)
-    # pwm.setPWMFreq(50)
+
     pwm.setPWM(0, 0, 4096)
-    pwm.close()
