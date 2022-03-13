c5173b14bc2f1f2bb53073bab6802bf4fa338d23
Shrish Premkrishna <shrish.premkrishna@gmail.com>
2022-03-13 11:21:04 -0700

    Update cv2test.py

--- cv2/cv2test.py
+++ cv2/cv2test.py
@@ -3,12 +3,17 @@ import numpy as np
 
 camera = cv2.VideoCapture(0)
 
-while True:
-    _, frame = camera.read()
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+# while True:
+#     _, frame = camera.read()
+#     cv2.imshow("Frame", frame)
+#     key = cv2.waitKey(1)
+#     if key == 27:
+#         break
 
+# camera.release()
+# cv2.destroyAllWindows()
+
+_, frame = camera.read()
+cv2.imwrite('opencv'+str(i)+'.png', image)
+cv2.imwrite('image', frame)
 camera.release()
-cv2.destroyAllWindows()
